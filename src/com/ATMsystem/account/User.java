c1f35701da619d43c3a51dfadf994c0126800ec3
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-03-28 13:42:47 +0800

    Update User.java
    随机给出11位卡号的creatID方法

--- src/com/ATMsystem/account/User.java
+++ src/com/ATMsystem/account/User.java
@@ -1,6 +1,15 @@
 package com.ATMsystem.account;
 
 public class User extends Account{
+    public static String creatID(){
+        Random random = new Random();
+        char[] ram = new char[11];
+        int i;
+        for (i = 0; i < 11; i++){
+            ram[i] = (char)(random.nextInt(10) + '0');
+        }
+        return new String(ram);
+    }
     public void freeze(){
         System.out.println("这里只有伪代码");
         //对于用户有冻结自己账号的功能
