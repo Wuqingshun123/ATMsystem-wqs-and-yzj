ec30a0c752e6b58853f9d2503d66e0711c45f4da
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-03-28 14:41:21 +0800

    用户属性public

--- src/com/ATMsystem/account/Account.java
+++ src/com/ATMsystem/account/Account.java
@@ -2,8 +2,8 @@ package com.ATMsystem.account;
 import java.util.*;
 import java.lang.*;
 public abstract class Account {
-    String card;//账号
-    StringBuffer password;//密码
+    public String card;//账号
+    public StringBuffer password;//密码
     public abstract void freeze();//冻结功能
     public abstract void exit();//退出登录功能
     public  abstract void changepassword();//修改密码功能
