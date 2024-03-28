f48a401f68f3e86c059ad26cfd73f4c7ad3921f1
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-03-28 15:28:25 +0800

    Update Account.java

--- src/com/ATMsystem/account/Account.java
+++ src/com/ATMsystem/account/Account.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.lang.*;
 public abstract class Account {
     public String card;//账号
-    public StringBuffer password;//密码
+    public StringBuffer password = new StringBuffer();//密码
     public abstract void freeze();//冻结功能
     public abstract void exit();//退出登录功能
     public  abstract void changepassword();//修改密码功能
