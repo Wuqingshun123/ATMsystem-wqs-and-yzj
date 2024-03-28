2d798b1918ea07a810aaf3c2a70c5459e95e38b8
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-03-28 13:32:23 +0800

    Update Main.java

--- src/com/ATMsystem/main/Main.java
+++ src/com/ATMsystem/main/Main.java
@@ -11,5 +11,9 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         Introduce.printintroduce();
         Wait.jixu();
+        System.out.println("1、登录");
+        System.out.println("2、注册");
+        System.out.println("3、忘记密码");
+        int key = scan.nextInt();
     }
 }
