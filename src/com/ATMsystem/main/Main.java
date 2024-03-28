038f46eac37bae2569c49a4775865b942063795f
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-03-28 15:33:03 +0800

    Update Main.java

--- src/com/ATMsystem/main/Main.java
+++ src/com/ATMsystem/main/Main.java
@@ -9,12 +9,12 @@ import java.util.*;
 import java.lang.*;
 
 public class Main {
-    public static int count = 0;
+    public static int count = 0;//用来记录有多少客户
     public static void main(String[] args) {
-        User[] users = new User[1000];
+        User[] users = new User[1000];//用来存储客户
         Scanner scan = new Scanner(System.in);
         Introduce.printintroduce();
         Wait.jixu();
-        Opration.initialoption(users, count);
+        Opration.initialoption(users, count);//初始界面
     }
 }
