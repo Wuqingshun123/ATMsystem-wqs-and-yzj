abda6f3b5bd5556646f7c17b243f24009d7a6f9d
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-03-28 13:20:31 +0800

    Add files via upload

---
+++ src/com/ATMsystem/account/Administor.java
@@ -0,0 +1,19 @@
+package com.ATMsystem.account;
+import java.util.*;
+import java.lang.*;
+import com.ATMsystem.account.Account;
+public class Administor extends Account{
+    public void freeze(){
+        System.out.println("这里只有伪代码");
+        //对于管理员有冻结他人账号的功能
+    }
+    public void exit(){
+        System.out.println("这里只有伪代码");
+        //退出管理员登录的功能
+    }
+
+    public void changepassword(){
+        System.out.println("这里只有伪代码");
+        //改变管理员密码的功能
+    }
+}
---
+++ src/com/ATMsystem/account/User.java
@@ -0,0 +1,17 @@
+package com.ATMsystem.account;
+
+public class User extends Account{
+    public void freeze(){
+        System.out.println("这里只有伪代码");
+        //对于用户有冻结自己账号的功能
+    }
+    public void exit(){
+        System.out.println("这里只有伪代码");
+        //退出用户登录的功能
+    }
+
+    public void changepassword(){
+        System.out.println("这里只有伪代码");
+        //修改用户密码的功能
+    }
+}
