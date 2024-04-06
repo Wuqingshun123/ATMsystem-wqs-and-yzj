e336e73c5e5c84b32bd23b86373e9399ef6651b6
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-04-06 15:23:41 +0800

    Update Account.java

--- src/com/ATMsystem/account/Account.java
+++ src/com/ATMsystem/account/Account.java
@@ -1,21 +1,13 @@
 package com.ATMsystem.account;
 import java.util.*;
 import java.lang.*;
-public class Account {
+public abstract class Account {
     public String card;//账号
     public StringBuffer password = new StringBuffer();//密码
-    public static void freeze(){
-        System.out.println("这里不会执行");
-    }//冻结功能
-    public static void exit(){
-        System.out.println("这里不会执行");
-    }//退出功能
+    public abstract void freeze();//冻结功能
+    public abstract void exit();//退出功能
 
-    public static void changepassword(){
-        System.out.println("这里不会执行");
-    }//修改密码功能
+    public abstract void changepassword();//修改密码功能
 
-    public static void cancelaccout(){
-        System.out.println("这里不会执行");
-    }//注销功能
+    public abstract void cancelaccout();//注销功能
 }
