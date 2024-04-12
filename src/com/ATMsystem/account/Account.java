bf55c5293e0efae6eb0c2ffc9ea845ea37b2bd77
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-04-12 14:31:51 +0800

    Update Account.java

--- src/com/ATMsystem/account/Account.java
+++ src/com/ATMsystem/account/Account.java
@@ -1,12 +1,13 @@
 package com.ATMsystem.account;
+import java.io.IOException;
 import java.util.*;
 import java.lang.*;
 public abstract class Account {
     public String card;//账号
     public StringBuffer password = new StringBuffer();//密码
-    public abstract void freeze(HashSet<User> users);//冻结功能
+    public abstract void freeze(HashSet<User> users, User user) throws IOException;//冻结功能
     public abstract void exit();//退出功能
 
 
-    public abstract void cancelaccout(HashSet<User> users);//注销功能
+    public abstract int cancelaccout(HashSet<User> users, User user) throws IOException;//注销功能
 }
