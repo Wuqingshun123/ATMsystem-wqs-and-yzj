b03f9026dfcbf7263c5c55acab2426482c82b585
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-04-12 15:02:29 +0800

    Update Recordwrite.java

--- src/com/data/record/Recordwrite.java
+++ src/com/data/record/Recordwrite.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class Recordwrite {
     public static void write(String str) throws IOException {
-        FileOutputStream fileOutputStream = new FileOutputStream("src\\data\\record\\traderecord.txt", true);
+        FileOutputStream fileOutputStream = new FileOutputStream("src\\com\\data\\record\\traderecord.txt", true);
         BufferedOutputStream f = new BufferedOutputStream(fileOutputStream);
         f.write(str.getBytes());
         f.close();
