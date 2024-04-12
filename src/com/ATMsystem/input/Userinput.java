8bac66527e572ed26bac548111ed994563821546
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-04-12 14:40:35 +0800

    Add files via upload

---
+++ src/com/ATMsystem/input/Userinput.java
@@ -0,0 +1,46 @@
+package com.ATMsystem.input;
+
+import com.ATMsystem.account.User;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Userinput {
+    public static void userinput(HashSet<User> users, User user) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        while(true){
+            System.out.printf("尊敬的 %s ,您好\n", user.name);
+            System.out.println("1、查询余额");
+            System.out.println("2、存款");
+            System.out.println("3、取款");
+            System.out.println("4、转账");
+            System.out.println("5、贷款");
+            System.out.println("6、还款");
+            System.out.println("7、查看交易记录");
+            System.out.println("8、修改密码");
+            System.out.println("9、冻结账号");
+            System.out.println("10、注销账号");
+            System.out.println("11、退出");
+            System.out.print("您的选择:");
+            int key = scan.nextInt();
+            System.out.println("-------------------------------------------------------------");
+            if (key == 1) user.check(user);
+            else if (key == 2) user.deposit(user);
+            else if (key == 3) user.withdraw(user);
+            else if (key == 4) user.transfer(users, user);
+            else if (key == 5) user.loan(user);
+            else if (key == 6) user.payback(user);
+            else if (key == 7) System.out.println("未实现");
+            else if (key == 8) user.changepassword(user);
+            else if (key == 9) {
+                user.freeze(null, user);
+                break;
+            }
+            else if (key == 10) {
+                if (user.cancelaccout(users, user) == 0) break;
+            }
+            else if (key == 11) user.exit();
+        }
+    }
+}
