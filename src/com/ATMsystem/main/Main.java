bf31a1d8775ae076a5599676d70a3aba04466afd
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-04-12 14:42:24 +0800

    Update Main.java

--- src/com/ATMsystem/main/Main.java
+++ src/com/ATMsystem/main/Main.java
@@ -5,14 +5,17 @@ import com.ATMsystem.account.User;
 import com.ATMsystem.input.Regist;
 import com.ATMsystem.interver.*;
 import com.ATMsystem.introduction.Introduce;
+import data.Time;
+import data.record.Recordwrite;
 
+import java.io.*;
 import java.util.*;
 import java.lang.*;
 
 public class Main {
     public static int count = 0;//用来记录有多少客户
     public static Administor admin = new Administor("admin", "admin");//创建管理员
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         HashSet<User> users = new HashSet<>();
         Scanner scan = new Scanner(System.in);
         Introduce.printintroduce();
