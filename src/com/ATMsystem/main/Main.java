38763b0bc1f43e878b08eec480c759dc08ebec54
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-04-14 00:04:59 +0800

    Update Main.java

--- src/com/ATMsystem/main/Main.java
+++ src/com/ATMsystem/main/Main.java
@@ -6,20 +6,31 @@ import com.ATMsystem.exception.Transfer_myself;
 import com.ATMsystem.input.Regist;
 import com.ATMsystem.interver.*;
 import com.ATMsystem.introduction.Introduce;
+import com.data.file_storage.AtmDao;
+import com.data.file_storage.FileLoadAndCommit;
+import com.data.Time;
 import com.data.record.Recordwrite;
 
 import java.io.*;
 import java.util.*;
 import java.lang.*;
 
+
 public class Main {
     public static int count = 0;//用来记录有多少客户
     public static Administor admin = new Administor("admin", "admin");//创建管理员
+    public static HashSet<User> users = new HashSet<>();
     public static void main(String[] args) throws IOException, Transfer_myself {
-        HashSet<User> users = new HashSet<>();
+        FileLoadAndCommit a = new FileLoadAndCommit(null);
+        users = a.loadFile();
         Scanner scan = new Scanner(System.in);
         Introduce.printintroduce();
         Wait.jixu();
         Opration.initialoption(users);//初始界面
+    }
+    public static void update() throws IOException {
+
+        AtmDao.updateDate(users);
+
     }
 }
