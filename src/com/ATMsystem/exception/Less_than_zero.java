c262bed37c9e2b3359ef43c849aa9a20a70b3fa5
Wuqingshun123 <165130959+Wuqingshun123@users.noreply.github.com>
2024-04-21 16:28:41 +0800

    Update Less_than_zero.java

--- src/com/ATMsystem/exception/Less_than_zero.java
+++ src/com/ATMsystem/exception/Less_than_zero.java
@@ -10,4 +10,7 @@ public class Less_than_zero extends Exception{
     public static void check(double money) throws Less_than_zero {
         if (money <= 0) throw new Less_than_zero("输入必须大于0");
     }
+    public static void check2( int money) throws Less_than_zero {
+        if (money < 0) throw new Less_than_zero("输入必须大于0");
+    }
 }
